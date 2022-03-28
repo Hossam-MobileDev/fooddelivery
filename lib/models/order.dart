4cc94296f6c4151695464d6d74b98d03b08423ce
Hossam-MobileDev <mmadooooo30@gmail.com>
2022-03-28 15:03:53 +0200

    Initial commit

---
+++ lib/models/order.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:fooddelivery/models/restaurant.dart';
+
+import 'food.dart';
+
+class Order {
+  final Restaurant restaurant;
+  final Food food;
+  final String date;
+  final int quantity;
+
+  Order({
+    required this.date,
+   required this.restaurant,
+    required this.food,
+  required  this.quantity,
+  });
+}
